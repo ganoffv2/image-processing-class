cc4b19a6dcd586bd4205a39110e833c963107635
unknown <208219@st.takushoku-u.ac.jp>
2023-05-11 10:56:43 +0900

    chenge imread argment

---
+++ classtest.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math>
+class complex {  // sample class for
+ private:
+  double real;  // 　real part
+  double imag;  // 　imagnary part
+ public:
+  complex() : real(-1.0), imag(-1.0) {  // comsractor
+  }
+  complex(double a, double b) : real(a), imag(b){};
+  double abs() { return sqrt(real * real + imag * imag); }
+};
+
+int main() {
+  // C++のクラスインスタンス
+  complex *a;
+  complex b[5, -7];
+  a = &b;
+  std::cout << "" << b.abs() << std::endl;
+}
--- imread.cpp
+++ imread.cpp
@@ -1,12 +1,25 @@
 #include <cstdio>
-#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
 
-int main(int argc ,char *argv[]) {
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    printf("Erro");
+    return EXIT_FAILURE;
+  }
   cv::Mat image;
   image = cv::imread(argv[1]);
   if (image.empty()) {
     printf("Image file is not found.\n");
     return EXIT_FAILURE;
+  }
+  printf("wdth = %d, hight = %d", image.cols, image.rows);
+  for (int y = 0; image.rows; ++y) {
+    for (int x = 0; image.cols; ++x) {
+      int val = image.data[y * image.cols + x];
+      val /= 2;
+      image.data[y * image.cols + x] = val;
+    }
   }
   cv::imshow("image", image);
   cv::waitKey();
